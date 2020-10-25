4fc2f947987146aa1f303b9892c66252a304ec1e
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2020-10-25 22:15:11 +0500

    feat: same brackets for opening and closing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const map = {};
+    const stack = [];
+    let item;
+    for(brackets in bracketsConfig) {
+          map[bracketsConfig[brackets][0]] = bracketsConfig[brackets][1]
+	}
+	
+	for(let i = 0; i < str.length; i++) {
+        item = str[i];
+        if(Object.keys(map).includes(map[item])){
+            if(stack.includes(map[item])){
+                stack.pop();
+                continue;
+            }
+            if(map[item]) {
+                stack.push(map[item])
+                continue;
+            }
+
+        }
+        else{
+            if(map[item]) {
+                stack.push(map[item])
+            }
+            else{
+                if(item !== stack.pop()){
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length === 0
+};
